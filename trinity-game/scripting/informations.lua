eb62123dc1e0d71f635a7e853dfdcc62f8d019b2
Kevin-Saukel <saukelkevin@gmail.com>
2016-09-06 21:06:35 +0200

    [06.09.2016 21:06]
    Update!

--- trinity-game/scripting/informations.lua
+++
@@ -1,8 +0,0 @@
-Server-Name: 	VIP Gaming
-Server-Farbe in dem das Design gehalten werden soll: Der Clan CC 00aa46 sollte eingebunden werden ansonsten schlicht wie im Video
-Scriptstil: 	Objekt-orientierte-Programmierung (OOP)
-Multiaccounts erlaubt: Multiaccounts sollten verboten werden + der Server wird später mit dem Forum verknüpft
-
-Gamemodeliste: Deathmatch, Hunter, Destruction Derby, FUN Mode, Old School Deathmatch, Schooter - sollte je nach update dann erweitert werden.
-
-Zusatfunktionen/Features für den jeweiligen Gamemode:
